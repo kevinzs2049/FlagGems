a33f212fe1ccde81186cae71da557ed94282b0ae
kevinzs2049 <kevinzs2049@gmail.com>
2026-01-16 17:34:25 +0800

    Neg: Optimize Performance for neg
    Average time lower down from 400us to 287us per time.
    Fix the accuracy issue for neg with bf16 test, usually like:
    test_unary_pointwise_ops.py::test_accuracy_neg[dtype2-shape0] python3: /home/runner/work/triton/triton/llvm-project/llvm/lib/CodeGen/SelectionDAG/LegalizeTypes.h:161: const llvm::SDValue& llvm::DAGTypeLegalizer::getSDValue(llvm::DAGTypeLegalizer::TableId&): Assertion Id && "TableId should be non-zero"' failed.
    Fatal Python error: Aborted
    
    For bf16 it will roll back to traditional realization by FlagGems common ops.
    
    Signed-off-by: kevinzs2049 <kevinzs2049@gmail.com>

--- src/flag_gems/runtime/backend/_arm/ops/__init__.py
+++ src/flag_gems/runtime/backend/_arm/ops/__init__.py
@@ -1,4 +1,4 @@
-from .add import add
+from .add import add, add_
 from .addmm import addmm
 from .all import all
 from .any import any
@@ -9,6 +9,16 @@ from .bmm import bmm
 from .cat import cat
 from .cos import cos
 from .cumsum import cumsum
+from .div import (
+    div_mode,
+    div_mode_,
+    floor_divide,
+    floor_divide_,
+    remainder,
+    remainder_,
+    true_divide,
+    true_divide_,
+)
 from .embedding import embedding
 from .exponential_ import exponential_
 from .full import full
@@ -20,13 +30,15 @@ from .log_softmax import log_softmax
 from .lt import lt
 from .masked_fill import masked_fill
 from .max import max
-from .mean import mean
+from .mean import mean, mean_dim
 from .min import min
 from .mm import mm
 from .multinomial import multinomial
+from .neg import neg, neg_
 from .ones import ones
 from .ones_like import ones_like
 from .quantile import quantile
+from .rsqrt import rsqrt, rsqrt_
 from .scatter import scatter
 from .silu import silu
 from .sin import sin
@@ -40,6 +52,7 @@ from .zeros import zeros
 
 __all__ = [
     "add",
+    "add_",
     "addmm",
     "all",
     "any",
@@ -49,8 +62,12 @@ __all__ = [
     "cat",
     "cos",
     "cumsum",
+    "div_mode",
+    "div_mode_",
     "embedding",
     "exponential_",
+    "floor_divide",
+    "floor_divide_",
     "full",
     "gather",
     "gelu",
@@ -61,12 +78,19 @@ __all__ = [
     "masked_fill",
     "max",
     "mean",
+    "mean_dim",
     "min",
     "mm",
     "multinomial",
+    "neg",
+    "neg_",
     "ones",
     "ones_like",
     "quantile",
+    "remainder",
+    "remainder_",
+    "rsqrt",
+    "rsqrt_",
     "scaled_dot_product_attention",
     "scatter",
     "silu",
@@ -76,6 +100,8 @@ __all__ = [
     "sub",
     "sum",
     "topk",
+    "true_divide",
+    "true_divide_",
     "where_self_out",
     "zeros",
 ]
