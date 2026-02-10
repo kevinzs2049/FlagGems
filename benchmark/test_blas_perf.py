e2b34cb338c510db0060b39eb01561e04d46cac6
Kevin Zhao <kevinzs2049@gmail.com>
2026-02-10 10:46:08 +0800

    arm64 cpu: update benchmark suite and related test/tool scripts

--- benchmark/test_blas_perf.py
+++ benchmark/test_blas_perf.py
@@ -1,19 +1,21 @@
-import os
+import itertools
 from typing import Generator
 
 import pytest
 import torch
 
 import flag_gems
-from benchmark.attri_util import (
+flag_gems.device = 'cpu'
+
+from .attri_util import (
     COMPLEX_DTYPES,
     DEFAULT_METRICS,
     FLOAT_DTYPES,
     BenchLevel,
-    model_shapes,
+    llama_shapes,
 )
-from benchmark.conftest import Config
-from benchmark.performance_utils import Benchmark, GenericBenchmark2DOnly, vendor_name
+from .conftest import Config
+from .performance_utils import Benchmark, vendor_name
 
 
 class BlasBenchmark(Benchmark):
@@ -29,24 +31,31 @@ class BlasBenchmark(Benchmark):
 
     def get_input_iter(self, cur_dtype) -> Generator:
         for b, m, n, k in self.shapes:
-            yield from self.input_fn(b, m, n, k, cur_dtype, self.device, False)
-
+            yield from self.input_fn(b, m, n, k, cur_dtype, self.device)
+        # llama shapes
         if Config.bench_level == BenchLevel.COMPREHENSIVE:
-            for b, m, n, k in self.shapes:
-                yield from self.input_fn(b, m, n, k, cur_dtype, self.device, True)
+            for m, n, k, _ in llama_shapes():
+                yield from self.input_fn(1, m, n, k, cur_dtype, self.device)
 
     def set_more_shapes(self):
-        large_k_shapes = [
-            (8, 1848, 1536, 151936),
-            (8, 1848, 1536, 128256),
-            (8, 1848, 1536, 152064),
-            (8, 4096, 1, 152064),
+        split_k_shapes = [
+            (1, m, m, k)
+            for m in [16 * i for i in range(1, 5)]
+            for k in [4096 * i for i in range(1, 9)]
         ]
-
-        model_shaps = model_shapes()
-        return large_k_shapes + model_shaps
+        # 'mv' operations only involve M and N dimensions.
+        # Shapes with large K values are not suitable for these two operations.
+        if self.op_name not in ["mv"]:
+            # B=1 or 4, M= 13, N= 2 , K=2^6..2^15
+            large_k_shapes = list(
+                itertools.product([1, 4], [13], [2], [2**i for i in range(6, 15)])
+            )
+            return large_k_shapes + split_k_shapes
+        return split_k_shapes
 
     def get_tflops(self, op, *args, **kwargs):
+        """This method is currently not really implemented and serves as a placeholder.
+        A proper implementation will be developed in the future."""
         total_flops = 0
         # shape(m,k)(k,n)
         # total_flops mxnx2k
@@ -54,12 +63,13 @@ class BlasBenchmark(Benchmark):
             total_flops = args[0].shape[0] * args[0].shape[1] * args[1].shape[1] * 2
         # shape(m,n)(n,p)
         # total_flops mxpx(2n+1)
-        elif self.op_name == "addmm":
+        if self.op_name == "addmm":
             total_flops = (
                 args[0].shape[0] * args[1].shape[1] * (args[1].shape[0] * 2 + 1)
             )
+        # shape(b,n,m), (b,m,p)
         # total_flops bxnxpx2m
-        elif self.op_name == "bmm":
+        if self.op_name == "bmm":
             total_flops = (
                 args[0].shape[0]
                 * args[0].shape[1]
@@ -67,191 +77,78 @@ class BlasBenchmark(Benchmark):
                 * 2
                 * args[0].shape[2]
             )
+        # shape(n,m)(m,)
+        # total_flops n*2m
+        if self.op_name == "mv":
+            total_flops = args[0].shape[0] * 2 * args[0].shape[1]
+
         return total_flops
 
 
-class BaddbmmBenchmark(BlasBenchmark):
-    """
-    benchmark for Baddbmm
-    """
-
-    def set_more_shapes(self):
-        model_shapes_list = model_shapes()
-
-        skip_shapes = [
-            (4, 8192, 128256, 4096),
-            (4, 8192, 152064, 3584),
-        ]
-
-        filtered = []
-        for shape in model_shapes_list:
-            if shape not in skip_shapes:
-                filtered.append(shape)
-
-        return filtered
-
-    def get_tflops(self, op, *args, **kwargs):
-        # shape(b,m,k)(b,k,n)
-        # total_flops = b * m * n * (2 * k + 1)
-        total_flops = (
-            args[1].shape[0]
-            * args[1].shape[1]
-            * args[2].shape[2]
-            * (args[1].shape[2] * 2 + 1)
-        )
-        return total_flops
-
-
-def addmm_input_fn(b, m, n, k, cur_dtype, device, b_column_major):
+def addmm_input_fn(b, m, n, k, cur_dtype, device):
     inp1 = torch.randn([m, k], dtype=cur_dtype, device=device)
+    inp2 = torch.randn([k, n], dtype=cur_dtype, device=device)
     bias = torch.randn([m, n], dtype=cur_dtype, device=device)
-    if b_column_major:
-        inp2 = torch.randn([n, k], dtype=cur_dtype, device=device)
-        yield bias, inp1, inp2.t(),
-    else:
-        inp2 = torch.randn([k, n], dtype=cur_dtype, device=device)
-        yield bias, inp1, inp2,
+    yield bias, inp1, inp2,
 
 
-def bmm_input_fn(b, m, n, k, cur_dtype, device, b_column_major):
+def bmm_input_fn(b, m, n, k, cur_dtype, device):
     inp1 = torch.randn([b, m, k], dtype=cur_dtype, device=device)
-    if b_column_major:
-        inp2 = torch.randn([b, n, k], dtype=cur_dtype, device=device)
-        yield inp1, inp2.transpose(1, 2)
-    else:
-        inp2 = torch.randn([b, k, n], dtype=cur_dtype, device=device)
-        yield inp1, inp2
+    inp2 = torch.randn([b, k, n], dtype=cur_dtype, device=device)
+    yield inp1, inp2
 
 
-def baddbmm_input_fn(b, m, n, k, cur_dtype, device, b_column_major):
-    inp1 = torch.randn([b, m, k], dtype=cur_dtype, device=device, requires_grad=True)
-
-    if b_column_major:
-        inp2 = torch.randn(
-            [b, n, k], dtype=cur_dtype, device=device, requires_grad=True
-        )
-        inp2 = inp2.transpose(1, 2).contiguous()
-    else:
-        inp2 = torch.randn(
-            [b, k, n], dtype=cur_dtype, device=device, requires_grad=True
-        )
-
-    bias = torch.randn([b, m, n], dtype=cur_dtype, device=device, requires_grad=True)
-
-    yield bias, inp1, inp2
-
-
-def mm_input_fn(b, m, n, k, cur_dtype, device, b_column_major):
+def mm_input_fn(b, m, n, k, cur_dtype, device):
     inp1 = torch.randn([m, k], dtype=cur_dtype, device=device)
-    if b_column_major:
-        inp2 = torch.randn([n, k], dtype=cur_dtype, device=device)
-        yield inp1, inp2.t()
-    else:
-        inp2 = torch.randn([k, n], dtype=cur_dtype, device=device)
-        yield inp1, inp2
+    inp2 = torch.randn([k, n], dtype=cur_dtype, device=device)
+    yield inp1, inp2
+
+
+def mv_input_fn(b, m, n, k, cur_dtype, device):
+    inp1 = torch.randn([m, n], dtype=cur_dtype, device=device)
+    inp2 = torch.randn([n], dtype=cur_dtype, device=device)
+    yield inp1, inp2
 
 
 @pytest.mark.parametrize(
-    "op_name, torch_op, input_fn, bench_cls",
+    "op_name, torch_op, input_fn",
     [
         pytest.param(
             "addmm",
             torch.addmm,
             addmm_input_fn,
-            BlasBenchmark,
             marks=pytest.mark.addmm,
         ),
         pytest.param(
             "bmm",
             torch.bmm,
             bmm_input_fn,
-            BlasBenchmark,
             marks=pytest.mark.bmm,
         ),
         pytest.param(
             "mm",
             torch.Tensor.mm,
             mm_input_fn,
-            BlasBenchmark,
             marks=pytest.mark.mm,
         ),
-        pytest.param(
-            "baddbmm",
-            torch.baddbmm,
-            baddbmm_input_fn,
-            BaddbmmBenchmark,
-            marks=pytest.mark.baddbmm,
-        ),
-    ],
-)
-def test_blas_benchmark(op_name, torch_op, input_fn, bench_cls):
-    if flag_gems.vendor_name == "mthreads" and op_name != "baddbmm":
-        os.environ["MUSA_ENABLE_SQMMA"] = "1"
-
-    bench = bench_cls(
-        input_fn=input_fn, op_name=op_name, torch_op=torch_op, dtypes=FLOAT_DTYPES
-    )
-    bench.run()
-
-    if flag_gems.vendor_name == "mthreads" and op_name != "baddbmm":
-        del os.environ["MUSA_ENABLE_SQMMA"]
-
-
-class MvAndOuterBenchmark(GenericBenchmark2DOnly):
-    """
-    Benchmark for MV and Outer operations
-    """
-
-    def set_more_shapes(self):
-        return None
-
-    def get_input_iter(self, cur_dtype) -> Generator:
-        for m, n in self.shapes:
-            yield from self.input_fn(m, n, cur_dtype, self.device)
-
-
-def mv_input_fn(m, n, cur_dtype, device):
-    inp1 = torch.randn([m, n], dtype=cur_dtype, device=device)
-    inp2 = torch.randn([n], dtype=cur_dtype, device=device)
-    yield inp1, inp2
-
-
-def outer_input_fn(m, n, cur_dtype, device):
-    inp1 = torch.randn([m], dtype=cur_dtype, device=device)
-    inp2 = torch.randn([n], dtype=cur_dtype, device=device)
-    yield inp1, inp2
-
-
-@pytest.mark.parametrize(
-    "op_name, torch_op, input_fn",
-    [
         pytest.param(
             "mv",
             torch.Tensor.mv,
             mv_input_fn,
             marks=pytest.mark.mv,
         ),
-        pytest.param(
-            "outer",
-            torch.Tensor.outer,
-            outer_input_fn,
-            marks=pytest.mark.outer,
-        ),
     ],
 )
-def test_mv_and_outer_benchmark(op_name, torch_op, input_fn):
-    bench = MvAndOuterBenchmark(
-        input_fn=input_fn,
-        op_name=op_name,
-        torch_op=torch_op,
-        dtypes=FLOAT_DTYPES,
+def test_blas_benchmark(op_name, torch_op, input_fn):
+    bench = BlasBenchmark(
+        input_fn=input_fn, op_name=op_name, torch_op=torch_op, dtypes=FLOAT_DTYPES
     )
     bench.run()
 
 
-class AddmvBenchmark(GenericBenchmark2DOnly):
+class OuterBenchmark(BlasBenchmark):
     """
-    Benchmark for addmv
+    benchmark for outer
     """
 
     def set_more_shapes(self):
@@ -262,30 +159,17 @@ class AddmvBenchmark(GenericBenchmark2DOnly):
             yield from self.input_fn(m, n, cur_dtype, self.device)
 
 
-def addmv_input_fn(m, n, cur_dtype, device):
-    mat = torch.randn([m, n], dtype=cur_dtype, device=device)
-    vec = torch.randn([n], dtype=cur_dtype, device=device)
-    bias = torch.randn([m], dtype=cur_dtype, device=device)
-    # torch.addmv(bias, mat, vec)
-    yield bias, mat, vec
+@pytest.mark.outer
+def test_outer_benchmark():
+    def outer_input_fn(m, n, cur_dtype, device):
+        inp1 = torch.randn([m], dtype=cur_dtype, device=device)
+        inp2 = torch.randn([n], dtype=cur_dtype, device=device)
+        yield inp1, inp2
 
-
-@pytest.mark.parametrize(
-    "op_name, torch_op, input_fn",
-    [
-        pytest.param(
-            "addmv",
-            torch.addmv,
-            addmv_input_fn,
-            marks=pytest.mark.addmv,
-        ),
-    ],
-)
-def test_addmv_benchmark(op_name, torch_op, input_fn):
-    bench = AddmvBenchmark(
-        input_fn=input_fn,
-        op_name=op_name,
-        torch_op=torch_op,
+    bench = OuterBenchmark(
+        input_fn=outer_input_fn,
+        op_name="outer",
+        torch_op=torch.Tensor.outer,
         dtypes=FLOAT_DTYPES,
     )
     bench.run()
@@ -305,7 +189,8 @@ class VdotBenchmark(BlasBenchmark):
             yield from self.input_fn(m, cur_dtype, self.device)
 
 
-@pytest.mark.skipif(vendor_name == "kunlunxin", reason="Random Hang")
+@pytest.mark.skipif(vendor_name == "kunlunxin", reason="RESULT TODOFIX")
+@pytest.mark.skipif(flag_gems.device == "musa", reason="Segmentation fault")
 @pytest.mark.vdot
 def test_vdot_benchmark():
     def vdot_input_fn(m, cur_dtype, device):
@@ -318,36 +203,5 @@ def test_vdot_benchmark():
         op_name="vdot",
         torch_op=torch.Tensor.vdot,
         dtypes=COMPLEX_DTYPES + FLOAT_DTYPES,
-    )
-    bench.run()
-
-
-class AddrBenchmark(BlasBenchmark):
-    """
-    benchmark for addr
-    """
-
-    def set_more_shapes(self):
-        return None
-
-    def get_input_iter(self, cur_dtype) -> Generator:
-        for shape in self.shapes:
-            m, n = shape[0], shape[1]
-            yield from self.input_fn(m, n, cur_dtype, self.device)
-
-
-@pytest.mark.addr
-def test_addr_benchmark():
-    def addr_input_fn(m, n, cur_dtype, device):
-        inp1 = torch.randn([m, n], dtype=cur_dtype, device=device)
-        inp2 = torch.randn([m], dtype=cur_dtype, device=device)
-        inp3 = torch.randn([n], dtype=cur_dtype, device=device)
-        yield inp1, inp2, inp3, {"alpha": 0.5, "beta": 0.5}
-
-    bench = AddrBenchmark(
-        input_fn=addr_input_fn,
-        op_name="addr",
-        torch_op=torch.Tensor.addr,
-        dtypes=FLOAT_DTYPES,
     )
     bench.run()
